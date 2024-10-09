f9b21a5ffa1d9dd8a0bf8057752ea4c2fb2275bc
Avijit Dey <97702087+avi-dey@users.noreply.github.com>
2024-10-09 10:06:11 +0530

    Delete standard DSA questions directory

--- standard DSA questions/array/easy/left_rotate.cpp
+++
@@ -1,4 +0,0 @@
-// https://www.naukri.com/code360/problems/rotate-array_1230543?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems&leftPanelTabValue=PROBLEM
-
-//copy of right_rotate, therefore left, check right_rotate
-
